b1b3d08baa3c8705bb3bcf5790391550bf182239
sergey radionov <rsatom@gmail.com>
2024-07-26 17:31:30 +0700

    RTCAudioSession: activationCount property moved to public API

--- sdk/objc/components/audio/RTCAudioSession+Private.h
+++ sdk/objc/components/audio/RTCAudioSession+Private.h
@@ -17,11 +17,6 @@ NS_ASSUME_NONNULL_BEGIN
 @interface RTC_OBJC_TYPE (RTCAudioSession)
 ()
 
-    /** Number of times setActive:YES has succeeded without a balanced call to
-     *  setActive:NO.
-     */
-    @property(nonatomic, readonly) int activationCount;
-
 /** The number of times `beginWebRTCSession` was called without a balanced call
  *  to `endWebRTCSession`.
  */
